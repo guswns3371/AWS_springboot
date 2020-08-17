39702bbf7c8d15eaf7461f34931d523db0c4b571
guswns <guswns3371@gmail.com>
2020-08-17 15:32:16 +0900

    gitignore 등록

---
+++ src/main/java/org/example/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,22 @@
+package org.example.springboot.config.auth.dto;
+
+import lombok.Getter;
+import org.example.springboot.domain.user.User;
+
+import java.io.Serializable;
+
+// User에 직렬화 하지 않고 따로 SessionUser 클래스를 만든 이유
+// User 클래스는 엔티티이기 떄문이다 => 자식 엔티티가 생길 경우, 자식들까지 직렬화 대상이 됨 => 성능 이슈, 부수 효과 발생한다.
+// 유지 보수 측면에서 직렬화 클래스를 따로 만드는게 도움 된다.
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { // Model 클래스 : 서버 템블릿 엔진에서 사용할 객체를 저장한다
         model.addAttribute("posts",postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // CustomAAuth2UserService에서 로그인 성공시 httpSession 세션에 SessionUser를 저장한다.
+        // 세션에 저장된 SessionUser객체를 getAttribute로 꺼내 user애 담는다
+
+        if (user != null) {
+            model.addAttribute("userName",user.getName());
+        }
+
         return "index";
     }
 
@@ -31,6 +44,6 @@ public class IndexController {
         model.addAttribute("post",dto);
 
         return "posts-update";
-
     }
+
 }
