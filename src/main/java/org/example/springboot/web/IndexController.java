c648d87d1d70a76645cd0ebcf6eff44a5d324710
guswns <guswns3371@gmail.com>
2020-08-20 00:11:09 +0900

    spring boot 2

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.LoginUser;
 import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
@@ -19,16 +20,20 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { // Model 클래스 : 서버 템블릿 엔진에서 사용할 객체를 저장한다
+    public String index(Model model, @LoginUser SessionUser user) { // Model 클래스 : 서버 템블릿 엔진에서 사용할 객체를 저장한다
         model.addAttribute("posts",postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        // CustomAAuth2UserService에서 로그인 성공시 httpSession 세션에 SessionUser를 저장한다.
-        // 세션에 저장된 SessionUser객체를 getAttribute로 꺼내 user애 담는다
-
         if (user != null) {
-            model.addAttribute("userName",user.getName());
+            model.addAttribute("userName", user.getName());
         }
+
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        // CustomAAuth2UserService에서 로그인 성공시 httpSession 세션에 SessionUser를 저장한다.
+//        // 세션에 저장된 SessionUser객체를 getAttribute로 꺼내 user애 담는다
+//
+//        if (user != null) {
+//            model.addAttribute("userName",user.getName());
+//        }
 
         return "index";
     }
